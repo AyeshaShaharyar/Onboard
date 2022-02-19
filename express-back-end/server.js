cd46b164441caacda9a5fb6c1f8702805d87f3cf
Luciana Viviani Gallo <lucianavivianigallo@gmail.com>
2022-02-19 14:26:57 +0000

    merging saturday and updating missing files

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -38,14 +38,14 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const employeesRoutes = require("./routes/employees");
-
+const adminRoutes = require("./routes/admin");
 const widgetsRoutes = require("./routes/widgets");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/employees", employeesRoutes(db));
-
+app.use("/api/admin", adminRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
